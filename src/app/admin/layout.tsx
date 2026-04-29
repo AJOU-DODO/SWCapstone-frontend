5bad517da13d2a3fcc803ecaa8c0a5e9267b0562
choihh2660 <choihh2660@ajou.ac.kr>
2026-04-29 12:43:23 +0900

    fix/#9:레이아웃 수정

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -9,28 +9,26 @@ const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata = {
   title: 'DODO 관리자 페이지',
-  description: 'Next.js와 TypeScript로 제작함',
+  description: 'DODO 서비스의 관리를 총괄합니다',
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode; // React 컴포넌트가 들어온다는 뜻의 타입
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="ko" className={cn("font-sans", geist.variable)}>
-      <body className="flex h-screen overflow-hidden"> 
-        {/* 1. 왼쪽 사이드바 영역 (로고 + 네비게이션) */}
-        <aside className="w-64 flex flex-col gap-3 items-center">
-          <Header /> {/* 로고가 들어있는 컴포넌트 */}
-          <NavigationBar />    {/* 메뉴 리스트가 들어있는 컴포넌트 */}
-        </aside>
+    <div className="flex h-screen overflow-hidden"> 
+      {/* 왼쪽 영역 (로고 + 메뉴) */}
+      <aside className="w-64 flex flex-col gap-3 items-center">
+        <Header /> 
+        <NavigationBar />
+      </aside>
 
-        {/* 2. 오른쪽 메인 콘텐츠 영역 */}
-        <main className="flex-1 h-screen overflow-y-auto">
-          {children}
-        </main>
-      </body>
-    </html>
+      {/* 오른쪽 영역 (기능 페이지) */}
+      <main className="flex-1 h-screen overflow-y-auto">
+        {children}
+      </main>
+    </div>
   );
 }
