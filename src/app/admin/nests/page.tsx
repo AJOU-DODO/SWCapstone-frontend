79dbbee88843a09edeca6f2bdfe48a5cfec2014b
choihh2660 <choihh2660@ajou.ac.kr>
2026-05-04 19:16:35 +0900

    refactor/#13: 정렬 버튼 컴포넌트 통합

--- src/app/admin/nests/page.tsx
+++ src/app/admin/nests/page.tsx
@@ -1,5 +1,7 @@
 import NestTabButton from '@/components/admin/NestTabButton';
 import NestTable, { Nest } from '@/components/admin/NestTable';
+import SortSection from '@/components/admin/SortSection';
+import Pagination from '@/components/admin/Pagination';
 
 export default async function Page({ searchParams, }: {searchParams: Promise<{ tab?: string }>;}) {
   //게시글 관리 메뉴 파라미터
@@ -10,7 +12,7 @@ export default async function Page({ searchParams, }: {searchParams: Promise<{ t
     { 
       id: "1",
       creatorNickname: "김도도",
-      content: "여기 벗쫓이 너무 예...",
+      content: "여기 벚쫓이 너무 예...",
       createdAt: "2023-02-01",
       likeCount: 37,
       replyCount: 5,
@@ -19,7 +21,7 @@ export default async function Page({ searchParams, }: {searchParams: Promise<{ t
     { 
       id: "2",
       creatorNickname: "김도도",
-      content: "여기 벗쫓이 너무 예...",
+      content: "여기 벚쫓이 너무 예...",
       createdAt: "2023-02-01",
       likeCount: 37,
       replyCount: 5,
@@ -28,7 +30,7 @@ export default async function Page({ searchParams, }: {searchParams: Promise<{ t
     { 
       id: "3",
       creatorNickname: "김도도",
-      content: "여기 벗쫓이 너무 예...",
+      content: "여기 벚쫓이 너무 예...",
       createdAt: "2023-02-01",
       likeCount: 37,
       replyCount: 5,
@@ -37,24 +39,56 @@ export default async function Page({ searchParams, }: {searchParams: Promise<{ t
     { 
       id: "4",
       creatorNickname: "김도도",
-      content: "여기 벗쫓이 너무 예...",
+      content: "여기 벚쫓이 너무 예...",
       createdAt: "2023-02-01",
       likeCount: 37,
       replyCount: 5,
       reportCount: 0,
     },
-    ]
+  ]
+  
+  const report: Nest[] = [
+    { 
+      id: "1",
+      creatorNickname: "김도도",
+      content: "안물어봤다요다야이야이야오",
+      createdAt: "2023-02-01",
+      likeCount: 37,
+      replyCount: 5,
+      reportCount: 14,
+    },
+  ]
+
+  //임의 데이터 (페이지네이션을 위한)
+  const totalItems = 80; // 전체 유저 수
+  const itemsPerPage = 10; // 한 페이지당 보여줄 수
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  //정렬옵션
+  const sortOptions = [
+    { label: "최신 순", value: "latest" },
+    { label: "좋아요 순", value: "likeCount" },
+    { label: "댓글 순", value: "replyCount" },
+  ];
 
   return (
-    <div>
+    <div className="grid grid-rows-[auto_auto_1fr_auto] p-10 pr-20 gap-8 h-screen overflow-hidden">
       <div>
         <NestTabButton/>
+      </div>
 
-        <div className="mt-4">
+      <div>
+        <SortSection options={sortOptions} defaultSort='latest'/>
+      </div>
+
+      <div className="mt-4">
         {activeTab === 'all' && <NestTable nests={nests}/>}
-        {activeTab === 'reported' && <div>신고된게시글</div>}
+        {activeTab === 'reported' && <NestTable nests={report}/>}
         {activeTab === 'comments' && <div>신고된댓글</div>}
-        </div>
+      </div>
+
+      <div className="mt-6 py-4 border-t">
+        <Pagination totalPages={totalPages}/>
       </div>
     </div>
   );
--- src/components/admin/NestTabButton.tsx
+++ src/components/admin/NestTabButton.tsx
@@ -13,10 +13,10 @@ export default function NestTabButton() {
   };
 
   const getTabStyle = (tab: string) => 
-    `rounded-sm transition-all border-2 p-5 pl-10 pr-10 border-[#54513E] bg-transparent flex items-center gap-1 ${activeTab === tab ? 'bg-[#54513E]/70 text-white hover:bg-[#54513E]/80 hover:text-white' : 'text-[#54513E] hover:bg-[#54513E]/30'}`;
+    `rounded-sm transition-all border-2 h-[7vh] pl-20 pr-20 border-[#54513E] bg-transparent flex items-center gap-1 ${activeTab === tab ? 'bg-[#54513E]/70 text-white hover:bg-[#54513E]/80 hover:text-white' : 'text-[#54513E] hover:bg-[#54513E]/30'}`;
 
   return (
-    <div className="flex gap-2 mb-6">
+    <div className="flex gap-2">
       <Button onClick={() => handleTabChange('all')} className={getTabStyle('all')}>
         전체
       </Button>
