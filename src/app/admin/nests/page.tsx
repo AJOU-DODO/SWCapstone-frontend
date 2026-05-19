a8d72e8d0601886784c89b15b5cc052feced79c4
choihh2660 <choihh2660@ajou.ac.kr>
2026-05-19 13:48:36 +0900

    feat/#13: 둥지 바디 컴포넌트 작성

--- src/app/admin/nests/page.tsx
+++ src/app/admin/nests/page.tsx
@@ -118,7 +118,7 @@ export default async function Page({ searchParams, }: {searchParams: Promise<{ t
         </div>
       </div>
 
-      <div className='w-full h-full grid grid-cols-[1fr_1fr]'>
+      <div className='w-full h-full grid grid-cols-[1fr_1fr] min-h-0 overflow-hidden'>
         <div className="">
           {activeTab === 'all' && <NestTable nests={nests}/>}
           {activeTab === 'reported' && <ReportNestTable nests={report}/>}
