2b3e264278e19700391b53c74d4e09b4db1661a7
defhoon <twilight22@ajou.ac.kr>
2026-05-29 23:49:46 +0900

    feat/#50: nestEditorStore 둥지 수정 페이지 관련 필드, 액션 추가

--- src/lib/store/nestEditorStore.ts
+++ src/lib/store/nestEditorStore.ts
@@ -16,6 +16,7 @@ interface NestEditorState {
   isSubmitting: boolean;
   errors: Partial<Record<keyof NestPayload, string>>;
   loadedDraftId: number | null;
+  loadedNestId: string | null;
 
   setBridgeData: (data: BridgeInitialData) => void;
   addImage: (url: string) => void;
@@ -33,6 +34,14 @@ interface NestEditorState {
   setLoadedDraftId: (id: number | null) => void;
   getDraftPayload: () => NestPayload;
   getPublishPayload: () => NestPayload | null;
+  setLoadedNestId: (id: string | null) => void;
+  setInitialNestData: (data: {
+    title: string;
+    content: string;
+    unlockRadius: 10 | 150;
+    categoryIds: number[];
+    imageUrls: string[];
+  }) => void;
 }
 
 export const useNestEditorStore = create<NestEditorState>((set, get) => ({
@@ -50,6 +59,7 @@ export const useNestEditorStore = create<NestEditorState>((set, get) => ({
   isSubmitting: false,
   errors: {},
   loadedDraftId: null,
+  loadedNestId: null,
 
   setBridgeData: (data) =>
     set({
@@ -77,6 +87,15 @@ export const useNestEditorStore = create<NestEditorState>((set, get) => ({
   setErrors: (errors) => set({ errors }),
   clearErrors: () => set({ errors: {} }),
   setLoadedDraftId: (id) => set({ loadedDraftId: id }),
+  setLoadedNestId: (id) => set({ loadedNestId: id }),
+  setInitialNestData: (data) =>
+    set({
+      title: data.title,
+      content: data.content,
+      unlockRadius: data.unlockRadius,
+      categoryIds: data.categoryIds,
+      imageUrls: data.imageUrls,
+    }),
 
   getDraftPayload: () => {
     const state = get();
