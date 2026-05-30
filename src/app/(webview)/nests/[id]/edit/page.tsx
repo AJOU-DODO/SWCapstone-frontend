4cbe678adf1b1e3566e72103703d74b9073283a7
defhoon <twilight22@ajou.ac.kr>
2026-05-31 00:01:58 +0900

    chore/#50: 둥지 수정 페이지 로드 테스트

--- src/app/(webview)/nests/[id]/edit/page.tsx
+++ src/app/(webview)/nests/[id]/edit/page.tsx
@@ -6,5 +6,5 @@ interface Props {
 
 export default async function NestEditPage({ params }: Props) {
   const { id } = await params;
-  return <NestUpdateClient nestId={id} />;
+  return <div>{id}</div>;
 }
