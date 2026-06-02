67130e109d8ea26f9dd631b95af2fceddbbecd5b
Rain-fore <choihh2660@naver.com>
2026-06-02 14:28:31 +0900

    feat/#58: 문의사항 테이블 구현

---
+++ src/components/admin/tables/InquiryTable.tsx
@@ -0,0 +1,68 @@
+//전체 둥지 관리 테이블
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+import { Inquiry } from '@/types/indexAdmin';
+
+interface TableProps {
+  inquiries: Inquiry[];
+  onRowClick: (id: number) => void;
+  selectedId: number | null;
+}
+
+export default function InquiryTable({ inquiries, onRowClick, selectedId }: TableProps) {
+  return (
+    <div className="border border-t-[#54513E] border-x-0 border-b-[#54513E]/50 [&_th]:text-center [&_td]:text-center">
+      <Table>
+        <colgroup>
+          <col className="w-[6%]" />
+          <col className="w-[18%]" />
+          <col className="w-[40%]" />
+          <col className="w-[8%]" />
+          <col className="w-[14%]" />
+          <col className="w-[14%]" />
+        </colgroup>
+
+        <TableHeader>
+          <TableRow className="border-[#54513E] hover:bg-transparent">
+            <TableHead>문의ID</TableHead>
+            <TableHead>작성자</TableHead>
+            <TableHead>제목</TableHead>
+            <TableHead>문의 상태</TableHead>
+            <TableHead>문의 날짜</TableHead>
+            <TableHead>답변 날짜</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {inquiries.map((inquiry) => {
+            const isSelected = selectedId === inquiry.id;
+            
+            return(
+            <TableRow 
+              key={inquiry.id} 
+              onClick={(e) => {
+                e.stopPropagation();
+                onRowClick(inquiry.id);
+              }}
+              className={`border-[#54513E]/50 cursor-pointer transition-colors hover:bg-[#54513E]/15 font-medium ${
+                inquiry.answeredAt ? "opacity-40" : ""
+              } `}>
+                <TableCell>{inquiry.id}</TableCell>
+                <TableCell>{inquiry.userNickname} {"(" + inquiry.userId + ")"}</TableCell>
+                <TableCell className="max-w-[150px] truncate">{"[" + inquiry.typeDescription + "]"} {inquiry.title}</TableCell>
+                <TableCell>{inquiry.statusDescription}</TableCell>
+                <TableCell>{new Date(inquiry.createdAt).toLocaleDateString()}</TableCell>
+                <TableCell>{new Date(inquiry.answeredAt).toLocaleDateString()}</TableCell>
+            </TableRow>
+          )})}
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
--- src/lib/adminApi/inquiry.ts
+++ src/lib/adminApi/inquiry.ts
@@ -1,7 +1,7 @@
 import api from '../axios';
 
 // 문의사항 목록 조회
-export const getInquiries = async (params?: { statuses?: string; sort?: string, page: number }) => {
+export const getInquiries = async (params?: { status?: string, page: number }) => {
   const { data } = await api.get('/api/v1/admin/inquiries', { params });
   return data;
 };
@@ -13,7 +13,7 @@ export const getInquiryDetail = async (inquiryId: number) => {
 };
 
 // 문의사항 답변 등록
-export const publishAnswer = async (inquiryId: number) => {
-  const { data } = await api.post(`/api/v1/admin/inquiries/${inquiryId}/answer`);
+export const publishAnswer = async (inquiryId: number, answer: string) => {
+  const { data } = await api.post(`/api/v1/admin/inquiries/${inquiryId}/answer`, { answer: answer });
   return data;
 };
