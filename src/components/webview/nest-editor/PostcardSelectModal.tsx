4a3870bc83819f8ae76d2222d54297b2f0878bc6
Rain-fore <choihh2660@naver.com>
2026-06-04 14:06:55 +0900

    fix/#65: dialog의 뷰포트 크기를 동적으로 참고하도록 변경

--- src/components/webview/nest-editor/PostcardSelectModal.tsx
+++ src/components/webview/nest-editor/PostcardSelectModal.tsx
@@ -64,7 +64,7 @@ export function PostcardSelectModal({
 
   return (
     <Dialog open={open} onOpenChange={(isOpen) => !isOpen && handleClose()}>
-      <DialogContent className="flex flex-col gap-0 p-0 bg-[#F7F4EC] border-[#E0DDD3] rounded-3xl max-h-[75vh] w-[calc(100vw-2rem)] max-w-md">
+      <DialogContent className="flex flex-col gap-0 p-0 bg-[#F7F4EC] border-[#E0DDD3] rounded-3xl max-h-[75dvh] w-[calc(100vw-2rem)] max-w-md">
         <DialogHeader className="px-5 pt-5 pb-4 border-b border-[#E0DDD3] shrink-0">
           <DialogTitle className="text-base font-semibold text-[#3D3830] text-left">
             엽서 선택
