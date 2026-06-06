e4b4ead1228b076790f6f5d3db7b04fff74badec
Rain-fore <choihh2660@naver.com>
2026-06-06 10:51:50 +0900

    feat/#68: 광고 승인 반려 모달 구현

--- src/components/admin/advertise/AdRequestDetailModal.tsx
+++ src/components/admin/advertise/AdRequestDetailModal.tsx
@@ -3,6 +3,7 @@
 import { PendingAdvertisement } from '@/types/indexAdmin';
 
 import ApproveButton from "@/components/admin/advertise/ApproveButton";
+import RejectButton from "@/components/admin/advertise/RejectButton";
 
 interface AdDetailModalProps {
   ad: PendingAdvertisement;
@@ -83,12 +84,13 @@ export default function AdDetailModal({ ad, onClose }: AdDetailModalProps) {
 
         {/* 하단 버튼 영역 (어드민 액션) */}
         <div className="flex justify-end gap-2 border-t border-gray-100 pt-3">
-          <button 
-            onClick={() => alert("거절 프로세스")} 
-            className="px-4 py-2 text-sm font-semibold text-rose-600 bg-rose-50 hover:bg-rose-100 rounded-xl transition-colors"
-          >
-            반려하기
-          </button>
+          <RejectButton 
+            adId={ad.id}
+            onSuccess={() => {
+              onClose();
+              window.location.reload();
+            }}
+          />
           <ApproveButton 
             adId={ad.id} 
             onSuccess={() => {
