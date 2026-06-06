4484fb849508d135f57a952c8bfeb77589344d93
Rain-fore <choihh2660@naver.com>
2026-06-06 16:06:42 +0900

    fix/#68: searchBar에서 useEffect 제거

--- src/components/admin/SearchBar.tsx
+++ src/components/admin/SearchBar.tsx
@@ -3,7 +3,7 @@
 import { Input } from "@/components/ui/input"
 import { Search } from "lucide-react"
 import { useRouter, useSearchParams } from "next/navigation";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 
 interface SearchBarProps {
   placeholder?: string; 
@@ -14,22 +14,24 @@ export default function SearchBar({ placeholder = "검색어를 입력하세요"
   const searchParams = useSearchParams();
   const debounceTimer = useRef<ReturnType<typeof setTimeout> | null>(null);
 
-  const [keyword, setKeyword] = useState(searchParams.get("search") || "");
-  const isTyping = useRef(false);
+  const urlKeyword = searchParams.get("search") || "";
+  const [keyword, setKeyword] = useState(urlKeyword);
+  
+  // 이전 URL 값을 기억할 Ref
+  const prevUrlKeyword = useRef(urlKeyword);
 
-  useEffect(() => {
-    if (!isTyping.current) {
-      setKeyword(searchParams.get("search") || "");
-    }
-  }, [searchParams]);
+  if (prevUrlKeyword.current !== urlKeyword) {
+    setKeyword(urlKeyword);
+    prevUrlKeyword.current = urlKeyword; // 동기화 후 마킹
+  }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    setKeyword(value);
-    isTyping.current = true;
+    setKeyword(value); // 내 input 창은 즉시 업데이트
 
     if (debounceTimer.current) clearTimeout(debounceTimer.current);
 
+    // 디바운스 시작
     debounceTimer.current = setTimeout(() => {
       const params = new URLSearchParams(searchParams.toString());
 
@@ -40,8 +42,8 @@ export default function SearchBar({ placeholder = "검색어를 입력하세요"
         params.delete("search");
       }
 
+      prevUrlKeyword.current = value.trim();
       router.push(`?${params.toString()}`);
-      isTyping.current = false;
     }, 500);
   };
 
@@ -54,7 +56,7 @@ export default function SearchBar({ placeholder = "검색어를 입력하세요"
         onChange={handleChange}
         className="h-[7vh] w-full pl-10 rounded-md border-[#2B6340] border-2 focus-visible:ring-1" 
       />
-      <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-[#2B6340] text-400" />
+      <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-[#2B6340]" />
     </div>
   )
 }
