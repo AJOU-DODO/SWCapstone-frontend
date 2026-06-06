cdb3bb48d6244de9b0adbceab1f6ea60e491c38b
Rain-fore <choihh2660@naver.com>
2026-06-06 11:44:10 +0900

    feat/#68: 광고 게시글 상세보기 페이지 구현

---
+++ src/components/admin/advertise/AdDetailHeader.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+import { AdNestDetailHeader } from '@/types/indexAdmin';
+import { useReportActions } from "@/hooks/admin/useReportActions";
+import DeleteModal from "@/components/admin/nest/DeleteModal";
+
+export default function AdDetailHeader ({ header }: { header: AdNestDetailHeader; }) {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+  const { handleNestDelete, isLoading: isDeleteLoading } = useReportActions({
+    targetId: header.nestId,
+    onSuccess: () => {
+      setIsDeleteModalOpen(false);
+    }
+  });
+
+  return (
+  <div className="flex flex-row justify-between items-start md:items-end gap-4 p-4 border-b bg-[#E8E4CD] w-full">
+    <div className="flex flex-row items-end gap-2 flex-shrink-0">
+      <div className="w-[50px] h-[50px] rounded-full overflow-hidden border-2 border-[#54513E] flex-shrink-0">
+        <Image 
+          src={header.profileImageUrl}
+          alt="DODO 로고"
+          width={50}
+          height={50}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="text-[#54513E] truncate font-medium whitespace-nowrap">
+        {header.authorNickname}
+      </div>
+      <div className="pl-3 text-xs text-gray-500 whitespace-nowrap pb-0.5">
+        {new Date(header.createdAt).toLocaleDateString()} {/*생성일*/}
+      </div>
+    </div>
+    <div className="flex flex-row gap-6 md:gap-8 text-xs text-gray-500 flex-1 justify-start md:justify-center whitespace-nowrap pb-0.5">
+      <div>
+        📍
+      </div>
+      <div>
+        {header.latitude.toFixed(4)}
+      </div>
+      <div>
+        {header.longitude.toFixed(4)}
+      </div>
+    </div>
+    
+    <div 
+    className="flex flex-row gap-2 items-end flex-shrink-0 w-full md:w-auto justify-end">
+      {!header.deleted && (
+        <>
+          <button 
+          onClick={() => setIsDeleteModalOpen(true)}
+          className="rounded-md border-2 h-9 w-16 border-black text-sm hover:bg-black/5 transition-colors">
+            삭제 
+          </button>
+        </>
+      )}
+
+      <DeleteModal
+        authorId={header.authorId}
+        isOpen={isDeleteModalOpen}
+        onClose={() => setIsDeleteModalOpen(false)}
+        onConfirm={handleNestDelete}
+        isLoading={isDeleteLoading}
+        title="게시물 삭제 확인"
+        message="정말로 이 콘텐츠를 삭제하시겠습니까?"
+      />
+    </div>
+  </div>
+  );
+};
